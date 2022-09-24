24654a6cc43d16b16af707aa3e2acd3106b52da4
jrkprogramming <jrkaibigan@gmail.com>
2022-09-24 16:54:14 -0500

    minor tweaks

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -14,31 +14,14 @@ const Skills = ({ directionLeft }: Props) => {
       className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Skills
+        Development Stack
       </h3>
 
-      <h3 className="absolute top-40 uppercase tracking-[3px] text-gray-500 text-xs">
+      {/* <h3 className="absolute top-40 uppercase tracking-[3px] text-gray-500 text-xs">
         my current development stack
-      </h3>
+      </h3> */}
 
       <div className="grid grid-cols-4 gap-5 mt-20">
-        {/* <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill /> */}
-
         <div className="relative flex cursor-pointer group">
           <motion.img
             initial={{
